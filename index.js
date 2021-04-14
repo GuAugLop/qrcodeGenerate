d92c82a513caba47f1d9fea12a4ce7f622f29759
Gustavo Lopes <gulopes.augusto@gmail.com>
2021-04-14 01:20:39 -0300

    otimizacoes

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var typeNumber = 6;
+var typeNumber = 1;
 var errorCorrectionLevel = "L";
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-function generate() {
+async function generate() {
+  var qr = qrcode(typeNumber, errorCorrectionLevel);
   const url = document.getElementById("url").value;
-  document.getElementById("qrcode").innerHTML = "";
   qr.addData(url);
-  qr.make();
-  document.getElementById("qrcode").innerHTML = qr.createImgTag(6, 6, 6);
+
+  try {
+    qr.make();
+    document.getElementById("qrcode").innerHTML = qr.createImgTag(6, 6, 6);
+    typeNumber = 1;
+  } catch (err) {
+    typeNumber += 1;
+    generate();
+  }
 }
